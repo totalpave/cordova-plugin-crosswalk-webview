4a16733a9e77c95d775f88993e551b5d322b76ff
Tyler Breau <tyler.breau@totalpave.com>
2019-08-20 17:32:26 -0300

    Refactored requireCordovaModule usage to use require if the package is not actually a cordova module.

--- hooks/after_build/000-build_64_bit.js
+++ hooks/after_build/000-build_64_bit.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+var deferral = require('q').defer();
+
 module.exports = function(context) {
 
     /** @external */
-    var deferral = context.requireCordovaModule('q').defer(),
-        UpdateConfig = require('./../update_config.js'),
+    // var deferral = context.requireCordovaModule('q').defer(),
+    var UpdateConfig = require('./../update_config.js'),
         updateConfig = new UpdateConfig(context);
 
     /** Main method */
